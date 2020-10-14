ee3c0ff20144e152448f0500f36f0c03186963c8
Pedro La Rosa <pedrolarosad@gmail.com>
2020-10-14 19:25:11 +0200

    audio player styled

--- src/components/audio-player/audio-player.tsx
+++ src/components/audio-player/audio-player.tsx
@@ -6,7 +6,22 @@ import {
 } from "components/icons";
 import { ScreenReaderOnly } from "components/screen-readers-helpers";
 import React, { FunctionComponent, MouseEvent, useRef, useState } from "react";
-import { NavigationButton } from "./audio-player-styled";
+import { ControlButtons, PlayerButton } from "./audio-player-styled";
+
+const ToggleReproduceButton: FunctionComponent<{ isPlaying: boolean }> = ({
+  isPlaying,
+}) =>
+  isPlaying ? (
+    <>
+      <PlayerPause data-testid="player-pause-icon" />
+      <ScreenReaderOnly>Pause</ScreenReaderOnly>
+    </>
+  ) : (
+    <>
+      <PlayerPlay data-testid="player-play-icon" />
+      <ScreenReaderOnly>Play</ScreenReaderOnly>
+    </>
+  );
 
 const AudioPlayer: FunctionComponent<AudioPlayerProps> = ({
   currentTrackURL,
@@ -17,57 +32,56 @@ const AudioPlayer: FunctionComponent<AudioPlayerProps> = ({
 }) => {
   const audioPlayerRef = useRef<HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleClickReproduceButton = async (
+    event: MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    isPlaying === true
+      ? await audioPlayerRef.current?.pause()
+      : await audioPlayerRef.current?.play();
+  };
+
+  const toggleIsPlaying = () => setIsPlaying(!isPlaying);
+
   return (
-    <div>
-      <NavigationButton
-        href={previousTrackPath}
-        data-testid="player-go-previous-button"
-        isDisabled={isPreviousButtonDisabled}
-      >
-        <PlayerPrevious isDisabled={isPreviousButtonDisabled} />
-        <ScreenReaderOnly>go Previous</ScreenReaderOnly>
-      </NavigationButton>
-      <button
-        data-testid="player-reproduce-button"
-        onClick={async (event: MouseEvent<HTMLButtonElement>) => {
-          event.preventDefault();
-          isPlaying === true
-            ? await audioPlayerRef.current?.pause()
-            : await audioPlayerRef.current?.play();
-        }}
-      >
-        {isPlaying ? (
-          <>
-            <PlayerPause data-testid="player-pause-icon" />
-            <ScreenReaderOnly>Pause</ScreenReaderOnly>
-          </>
-        ) : (
-          <>
-            <PlayerPlay data-testid="player-play-icon" />
-            <ScreenReaderOnly>Play</ScreenReaderOnly>
-          </>
-        )}
-      </button>
-      <NavigationButton
-        href={nextTrackPath}
-        data-testid="player-go-next-button"
-        isDisabled={isNextButtonDisabled}
-      >
-        <PlayerNext isDisabled={isNextButtonDisabled} />
-        <ScreenReaderOnly>go next</ScreenReaderOnly>
-      </NavigationButton>
+    <>
+      <ControlButtons>
+        <PlayerButton
+          href={previousTrackPath}
+          data-testid="player-go-previous-button"
+          isDisabled={isPreviousButtonDisabled}
+        >
+          <PlayerPrevious isDisabled={isPreviousButtonDisabled} />
+          <ScreenReaderOnly>go Previous</ScreenReaderOnly>
+        </PlayerButton>
+
+        <PlayerButton
+          as="button"
+          data-testid="player-reproduce-button"
+          onClick={handleClickReproduceButton}
+        >
+          <ToggleReproduceButton isPlaying={isPlaying} />
+        </PlayerButton>
+
+        <PlayerButton
+          href={nextTrackPath}
+          data-testid="player-go-next-button"
+          isDisabled={isNextButtonDisabled}
+        >
+          <PlayerNext isDisabled={isNextButtonDisabled} />
+          <ScreenReaderOnly>go next</ScreenReaderOnly>
+        </PlayerButton>
+
+      </ControlButtons>
       {currentTrackURL && (
         <audio
           data-testid="player-audio-element"
           controls
           hidden
           ref={audioPlayerRef}
-          onPause={() => {
-            setIsPlaying(false);
-          }}
-          onPlaying={() => {
-            setIsPlaying(true);
-          }}
+          onPause={toggleIsPlaying}
+          onPlaying={toggleIsPlaying}
         >
           <source
             data-testid="player-source"
@@ -76,7 +90,7 @@ const AudioPlayer: FunctionComponent<AudioPlayerProps> = ({
           />
         </audio>
       )}
-    </div>
+    </>
   );
 };
 
