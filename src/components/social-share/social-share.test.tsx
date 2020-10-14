8e1ac5c88b0789f1e57e791506eee196f701744a
Pedro La Rosa <pedrolarosad@gmail.com>
2020-10-14 23:44:48 +0200

    social-share component tested

--- src/components/social-share/social-share.test.tsx
+++ src/components/social-share/social-share.test.tsx
@@ -17,33 +17,49 @@ describe("Testing the social share component", () => {
 
   let randomURL: string;
 
-  beforeEach(() => {
-    randomURL = Random.getString();
+  describe("shareURL is empty (default value)", () => {
 
-    render(<SocialShare shareURL={randomURL}/>);
+    it("the not add any share url to the links when the shareURL parameter is not passed", () => {
+      render(<SocialShare />);
+      const {getByTestId} = screen;
+      
+      expect(getByTestId(facebookLinkTestId).href).toBe(shareURLs.facebookURL);
+      expect(getByTestId(twitterLinkTestId).href).toBe(shareURLs.twitterURL);
+      expect(getByTestId(whatsappLinkTestId).href).toBe(shareURLs.whatsappURL);
+    })
   })
 
-  it("should show the correct url, icon and hidden message for the facebook Link", () => {
-    const {getByTestId} = screen;
-
-    expect(getByTestId(facebookLinkTestId).href).toBe(`${shareURLs.facebookURL}${randomURL}`);
-    expect(getByTestId(facebookHiddenMessageTestid).textContent).toBe(`${hiddenMessages.facebookHiddenMessage}`);
-    expect(getByTestId(facebookIconTestid)).toBeInTheDocument();
-  });
-
-  it("should show the correct url, icon and hidden message for the twitter Link", () => {
-    const {getByTestId} = screen;
-
-    expect(getByTestId(twitterLinkTestId).href).toBe(`${shareURLs.twitterURL}${randomURL}`);
-    expect(getByTestId(twitterHiddenMessageTestid).textContent).toBe(`${hiddenMessages.twitterHiddenMessage}`);
+  describe("specifying the shareURL property", ()=> {
+    
+    beforeEach(() => {
+      randomURL = Random.getString();
+      
+      render(<SocialShare shareURL={randomURL}/>);
+    });
+    
+    it("should show the correct url, icon and hidden message for the facebook Link", () => {
+      const {getByTestId} = screen;
+      
+      expect(getByTestId(facebookLinkTestId).href).toBe(`${shareURLs.facebookURL}${randomURL}`);
+      expect(getByTestId(facebookHiddenMessageTestid).textContent).toBe(`${hiddenMessages.facebookHiddenMessage}`);
+      expect(getByTestId(facebookIconTestid)).toBeInTheDocument();
+    });
+    
+    it("should show the correct url, icon and hidden message for the twitter Link", () => {
+      const {getByTestId} = screen;
+      
+      expect(getByTestId(twitterLinkTestId).href).toBe(`${shareURLs.twitterURL}${randomURL}`);
+      expect(getByTestId(twitterHiddenMessageTestid).textContent).toBe(`${hiddenMessages.twitterHiddenMessage}`);
     expect(getByTestId(twitterIconTestid)).toBeInTheDocument();
   });
-
+  
   it("should show the correct url, icon and hidden message for the whatsapp Link", () => {
     const {getByTestId} = screen;
-
+    
     expect(getByTestId(whatsappLinkTestId).href).toBe(`${shareURLs.whatsappURL}${randomURL}`);
     expect(getByTestId(whatsappHiddenMessageTestid).textContent).toBe(`${hiddenMessages.whatsappHiddenMessage}`);
     expect(getByTestId(whatsappIconTestid)).toBeInTheDocument();
   });
 });
+
+})
